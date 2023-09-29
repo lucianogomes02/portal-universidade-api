427ce5baff23a42016fe9319981d9810122e041c
lucianogomes02 <lucianogvda02@gmail.com>
2023-09-29 14:19:15 -0300

    feat(src/users): implements ProfessorSerializer and ProfessorService

---
+++ src/users/service/professor/services.py
@@ -0,0 +1,45 @@
+from typing import Union
+
+from rest_framework.response import Response
+
+from src.users.models import Professor
+from src.users.repository.professor_repository import ProfessorRepository
+from src.users.service.professor.serializers import ProfessorSerializer
+
+
+class ProfessorService:
+    @staticmethod
+    def search_for_professor(professor_id) -> Response:
+        professor = ProfessorRepository().search_by_id(professor_id=professor_id)
+        if not professor:
+            return Response({"message": "Professor não encontrado"})
+        serializer = ProfessorSerializer(professor)
+        return Response(serializer.data)
+
+    @staticmethod
+    def register_professor(request_data) -> Union[Response, Professor]:
+        serializer = ProfessorSerializer(data=request_data)
+        if serializer.is_valid():
+            professor_data = serializer.validated_data
+            professor = ProfessorRepository().save(professor_data=professor_data)
+            return professor
+        return Response(serializer.errors, status=400)
+
+    @staticmethod
+    def change_professor_registry(
+        professor_id, request_data
+    ) -> Union[Response, Professor]:
+        professor = ProfessorRepository().search_by_id(professor_id=professor_id)
+        serializer = ProfessorSerializer(instance=professor, data=request_data)
+        if serializer.is_valid():
+            professor_changed = ProfessorRepository().update(
+                professor=professor, updated_data=request_data
+            )
+            return professor_changed
+        return Response(serializer.errors, status=400)
+
+    @staticmethod
+    def unregister_professor(professor_id):
+        professor = ProfessorRepository().search_by_id(professor_id=professor_id)
+        if professor:
+            ProfessorRepository().delete(professor=professor)
