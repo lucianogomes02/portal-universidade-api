7238b4af5626fd28282b20d123f18bc9adf18c7f
lucianogomes02 <lucianogvda02@gmail.com>
2023-09-29 14:44:39 -0300

    feat(src/users): implements StudentSerializer

---
+++ src/users/service/student/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+
+from src.users.models import Student
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True)
+    name = serializers.CharField(required=False)
+    email = serializers.EmailField(required=False)
+    password = serializers.CharField(write_only=True, required=False)
+    birth_date = serializers.DateField(required=False)
+
+    class Meta:
+        model = Student
+        fields = ["id", "name", "email", "password", "birth_date"]
+
+    def validate(self, data):
+        if self.instance:
+            return data
+
+        model_fields = self.Meta.fields.copy()
+        model_fields.pop(0)
+        for field in model_fields:
+            if field not in data:
+                raise serializers.ValidationError({field: "Este campo é obrigatório."})
+
+        return data
