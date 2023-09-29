2f8973f51605f95e8bfb66894464e4aeac55df06
lucianogomes02 <lucianogvda02@gmail.com>
2023-09-29 11:59:56 -0300

    refactor(src/users): rollback CoordinatorSerializer to ModelSerializer

--- src/users/service/serializers.py
+++ src/users/service/serializers.py
@@ -1,21 +1,12 @@
 from rest_framework import serializers
 
-from src.users.domain.entities import Coordinator
+from src.users.models import CoordinatorUser
 
 
-class CoordinatorSerializer(serializers.Serializer):
+class CoordinatorSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(read_only=True)
-    name = serializers.CharField(required=False)
-    email = serializers.EmailField(required=False)
-    password = serializers.CharField(write_only=True, required=False)
-    birth_date = serializers.DateField(required=False)
+    password = serializers.CharField(write_only=True)
 
-    def create(self, validated_data):
-        return Coordinator(**validated_data)
-
-    def update(self, instance, validated_data):
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-
-        instance.save()
-        return instance
+    class Meta:
+        model = CoordinatorUser
+        fields = ["id", "name", "email", "password", "birth_date"]
