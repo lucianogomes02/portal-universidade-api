942c5c63d45800a72a4de4e0a5ee2a9cd72acb89
Luciano Gomes <lucianogvda02@gmail.com>
2023-09-30 19:35:36 -0300

    feat(src/courses): implements Course model

--- src/courses/models.py
+++ src/courses/models.py
@@ -1,3 +1,18 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from src.users.models import Professor, Student
+
+
+class Course(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    name = models.CharField(max_length=100)
+    workload = models.PositiveIntegerField()
+    professor = models.ForeignKey(
+        Professor, on_delete=models.CASCADE, related_name="professors_courses"
+    )
+    students = models.ManyToManyField(Student, related_name="students_courses")
+
+    def __str__(self):
+        return self.name
