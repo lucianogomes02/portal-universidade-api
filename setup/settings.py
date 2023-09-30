c5d737a9b391ec74df2bd196fbe5aa7ec3626475
Luciano Gomes <lucianogvda02@gmail.com>
2023-09-30 15:05:18 -0300

    feat(setup/settings): define IsAuthenticated as DEFAULT permission class

--- setup/settings.py
+++ setup/settings.py
@@ -130,4 +130,5 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
     ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
