8446953b4984fc9d74dcf945f4752e2b2a131492
Luciano Gomes <lucianogvda02@gmail.com>
2023-10-01 03:22:30 -0300

    chore(src/courses): configure django admin models

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from src.courses.models import Course
+
+admin.site.register(Course)
--- src/users/admin.py
+++ src/users/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Student, Professor, Coordinator
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Student)
+admin.site.register(Professor)
+admin.site.register(Coordinator)
