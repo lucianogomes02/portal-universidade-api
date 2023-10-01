a00d44311a3193f17b49e307424bc43b4c4125e7
Luciano Gomes <lucianogvda02@gmail.com>
2023-10-01 04:58:28 -0300

    feat(src/grades): implements GradeRepository

---
+++ src/grades/repository/grade_repository.py
@@ -0,0 +1,40 @@
+from typing import Dict, Optional
+from uuid import UUID
+
+from django.db.models import QuerySet
+
+from src.grades.models import Grade
+from src.libs.repository import Repository
+
+
+class GradeRepository(Repository):
+    def __init__(self):
+        self.grade = Grade
+
+    def search_all_objects(self) -> QuerySet[Grade]:
+        return self.grade.objects.all()
+
+    def search_by_id(self, grade_id: UUID) -> Grade:
+        return self.grade.objects.filter(id=grade_id).first()
+
+    def save(self, grade_data: Dict) -> Grade:
+        grade = Grade.objects.create(
+            name=grade_data.get("name"),
+            course=grade_data.get("course"),
+            professor=grade_data.get("professor"),
+            student=grade_data.get("student"),
+            value=grade_data.get("value"),
+        )
+        grade.save()
+        return grade
+
+    def update(self, grade: Grade, updated_data: Optional[Dict] = None) -> Grade:
+        if updated_data:
+            for key, value in updated_data.items():
+                if hasattr(grade, key) and getattr(grade, key) != value:
+                    setattr(grade, key, value)
+        grade.save()
+        return grade
+
+    def delete(self, grade: Grade):
+        grade.delete()
