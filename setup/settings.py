202a8f3d0098c90f96f546aa53cba49bb2322abc
Luciano Gomes <lucianogvda02@gmail.com>
2023-10-01 04:23:26 -0300

    feat(src/grades): grades context initial commit

--- setup/settings.py
+++ setup/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "src.users",
     "src.courses",
+    "src.grades",
 ]
 
 MIDDLEWARE = [
---
+++ src/grades/models.py
@@ -0,0 +1,23 @@
+import uuid
+
+from django.db import models
+
+from src.courses.models import Course
+from src.users.models import Professor, Student
+
+
+class Grade(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, related_name="courses_grades"
+    )
+    professor = models.ForeignKey(
+        Professor, on_delete=models.CASCADE, related_name="professors_grades"
+    )
+    student = models.ForeignKey(
+        Student, on_delete=models.CASCADE, related_name="students_grades"
+    )
+    value = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.student} - {self.course} - {self.value}"
