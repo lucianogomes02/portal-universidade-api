170a44478dca0722fb61e817121ed7fc23cece44
Luciano Gomes <lucianogvda02@gmail.com>
2023-10-02 02:54:10 -0300

    test(src/courses): implements courses api tests

---
+++ src/courses/tests/test_courses_api.py
@@ -0,0 +1,89 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from src.courses.repository.course_repository import CourseRepository
+from src.users.models import User
+from src.users.repository.professor_repository import ProfessorRepository
+from src.users.repository.student_repository import StudentRepository
+
+
+class CoursesAPITestCase(APITestCase):
+    def setUp(self):
+        self.courses_list = reverse("Courses-list")
+        self.super_user = User.objects.create_superuser(
+            name="Superuser Test",
+            email="superusertest@example.com",
+            password="1234",
+            username="Superuser Test",
+            birth_date="2000-01-01",
+        )
+        self.client.force_authenticate(self.super_user)
+
+        self.professor = ProfessorRepository().save(
+            {
+                "name": "Test Professor",
+                "email": "professortest@example.com",
+                "password": "1234",
+                "birth_date": "2000-01-01",
+            }
+        )
+
+        self.student = StudentRepository().save(
+            {
+                "name": "Test Student",
+                "email": "studenttest@example.com",
+                "password": "1234",
+                "birth_date": "2000-01-01",
+            }
+        )
+
+        course_data = {
+            "name": "Course Test",
+            "professor": self.professor,
+            "workload": 100,
+        }
+
+        self.course = CourseRepository().save(
+            {
+                "name": "Course Test",
+                "professor": self.professor,
+                "students": [self.student],
+                "workload": 100,
+            }
+        )
+
+    def test_get_courses(self):
+        response = self.client.get(self.courses_list)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_post_courses(self):
+        course_data = {
+            "name": "Course Test 2",
+            "professor": self.professor.id,
+            "workload": 50,
+        }
+
+        response = self.client.post(self.courses_list, data=course_data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data.get("message"), "Disciplina criada com sucesso")
+
+    def test_put_course(self):
+        course_change_data = {
+            "name": "Teste Course Changed",
+        }
+        response = self.client.put(
+            f"/api/courses/{self.course.id}/", data=course_change_data
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data.get("message"), "Disciplina atualizada com sucesso"
+        )
+
+    def test_delete_course(self):
+        response = self.client.delete(f"/api/courses/{self.course.id}/")
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        self.assertEqual(
+            response.data.get("message"), "Disciplina removida com sucesso"
+        )
