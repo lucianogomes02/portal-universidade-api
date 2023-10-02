8c2b7007a11de1a456e8f44a6af3c517d1d5458e
Luciano Gomes <lucianogvda02@gmail.com>
2023-10-02 02:06:47 -0300

    test(src/courses): implements courses services test case

---
+++ src/courses/tests/test_courses_services.py
@@ -0,0 +1,95 @@
+from unittest.mock import Mock
+
+from django.test import TestCase
+from rest_framework import status
+
+from src.courses.models import Course
+from src.courses.repository.course_repository import CourseRepository
+from src.courses.service.course.services import CourseService
+from src.users.repository.professor_repository import ProfessorRepository
+from src.users.repository.student_repository import StudentRepository
+
+
+class CoursesServiceTestCase(TestCase):
+    def setUp(self):
+        self.professor = ProfessorRepository().save(
+            {
+                "name": "Test Professor",
+                "email": "professortest@example.com",
+                "password": "1234",
+                "birth_date": "2000-01-01",
+            }
+        )
+
+        self.student = StudentRepository().save(
+            {
+                "name": "Test Student",
+                "email": "studenttest@example.com",
+                "password": "1234",
+                "birth_date": "2000-01-01",
+            }
+        )
+
+        self.course = CourseRepository().save(
+            {
+                "name": "Course Test",
+                "professor": self.professor,
+                "students": [self.student],
+                "workload": 100,
+            }
+        )
+
+        self.course_data = {
+            "name": "Course Test",
+            "professor": self.professor.id,
+            "workload": 100,
+        }
+
+    def test_search_for_course(self):
+        CourseRepository = Mock()
+        self.course_repository = CourseRepository
+
+        self.course_repository.return_value.search_by_id.return_value = self.course
+
+        response = CourseService.search_for_course(course_id=self.course.id)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["name"], "Course Test")
+
+    def test_register_course(self):
+        CourseRepository = Mock()
+        self.course_repository = CourseRepository
+
+        self.course_repository.return_value.save.return_value = Course(
+            **{
+                "name": "Course Test",
+                "professor": self.professor,
+                "workload": 100,
+            }
+        )
+
+        response = CourseService.register_course(self.course_data)
+
+        self.assertTrue(isinstance(response, Course))
+        self.assertEqual(response.name, "Course Test")
+
+    def test_unregister_course(self):
+        CourseRepository = Mock()
+        self.course_repository = CourseRepository
+
+        self.course_repository.return_value.save.return_value = Course(
+            **{
+                "name": "Course Test",
+                "professor": self.professor,
+                "workload": 100,
+            }
+        )
+
+        course = CourseService.register_course(self.course_data)
+
+        self.course_repository.return_value.search_by_id.return_value = self.course
+
+        response = CourseService.unregister_course(course_id=self.course.id)
+
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        self.assertEqual(response.data["message"], "Disciplina removida com sucesso")
