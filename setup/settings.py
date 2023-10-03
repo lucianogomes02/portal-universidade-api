f725322fba0502b59a2814d1157dc6f7295e0454
Luciano Gomes <lucianogvda02@gmail.com>
2023-10-02 21:27:49 -0300

    feat(setup/settings): implements pagination configs

--- setup/settings.py
+++ setup/settings.py
@@ -137,4 +137,6 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
         "rest_framework.permissions.DjangoModelPermissions",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
